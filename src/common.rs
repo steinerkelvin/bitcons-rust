aacc01408c7770aa7d98d5003c2fa39bb05eafff
Kelvin Santos <kelvinsteinersantos@gmail.com>
2021-08-31 11:51:14 -0300

    implements Serialize for Body/Post

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,32 @@
 use primitive_types::U256;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 
-type Body = [u8; 1024];
+#[derive(Debug, Clone)]
+struct Body {
+    val: [u8; 1024],
+}
 
-#[derive(Debug)] # Deserialize, Serialize
+impl Serialize for Body {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        use serde::ser::SerializeSeq;
+        let mut seq_ser = serializer.serialize_seq(Some(1024))?;
+        for i in 0..(self.val.len()) {
+            seq_ser.serialize_element(&self.val[i])?;
+        }
+        seq_ser.end()
+    }
+}
+
+impl From<[u8; 1024]> for Body {
+    fn from(val: [u8; 1024]) -> Self {
+        Body { val }
+    }
+}
+
+#[derive(Debug, Serialize)]
 pub struct Post {
     prev: U256, // previous post (32 bytes)
     work: U256, // extra info and nonce (32 bytes)
@@ -28,7 +51,7 @@ mod tests {
     use primitive_types::U256;
     use rand::prelude::*;
 
-    use super::Post;
+    use super::{Body, Post};
 
     #[test]
     fn post_hash() {
@@ -37,7 +60,7 @@ mod tests {
         let buf = rng.gen::<[u8; 32]>();
         let work = U256::from_little_endian(&buf);
 
-        let body = [42; 1024];
+        let body: Body = Body::from([42; 1024]);
 
         let post = Post::new(&prev, &work, &body);
         // println!("POST: {:?}", post);
