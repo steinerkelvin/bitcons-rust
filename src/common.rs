cf7c51e964b46a29d22e1d27b971544f6df51785
Kelvin Santos <kelvinsteinersantos@gmail.com>
2021-09-06 16:21:29 -0300

    deserializing addresses

--- src/common.rs
+++ src/common.rs
@@ -10,8 +10,10 @@ pub const BODY_SIZE: usize = 1024;
 pub const POST_SIZE: usize = 2 * WORD_SIZE + BODY_SIZE;
 
 // ipv6 address size
-pub const ADDRESS_SIZE: usize = 16;
-// pub const ADDRESS_LIST_SIZE = |x: usize| x + 1 * ADDRESS_SIZE;
+pub const IPV6_SIZE: usize = 16;
+pub const PORT_SIZE: usize = 2;
+pub const ADDRESS_SIZE: usize = IPV6_SIZE + PORT_SIZE;
+// pub const ADDRESS_LIST_SIZE = |x: usize| 1 + x * ADDRESS_SIZE;
 
 // Post Body
 
@@ -126,6 +128,7 @@ fn hash_score(hash: U256) -> U256 {
 
 // Address
 
+#[derive(Debug, PartialEq)]
 enum Address {
     IP(std::net::IpAddr, u16),
 }
@@ -150,6 +153,37 @@ impl<'a> Ser<'a> for Address {
     }
 }
 
+impl<'a> Deser<'a> for Address {
+    fn deser_from_iter<I>(it: &mut I) -> Result<Self, DeserError>
+    where
+        I: Iterator<Item = u8>,
+    {
+        let ip_iter = it.take(IPV6_SIZE);
+        let ip_vec: Vec<u8> = ip_iter.collect();
+        // TODO check size
+        let s0: u16 = (ip_vec[00] as u16) << 8 | (ip_vec[01] as u16);
+        let s1: u16 = (ip_vec[02] as u16) << 8 | (ip_vec[03] as u16);
+        let s2: u16 = (ip_vec[04] as u16) << 8 | (ip_vec[05] as u16);
+        let s3: u16 = (ip_vec[06] as u16) << 8 | (ip_vec[07] as u16);
+        let s4: u16 = (ip_vec[08] as u16) << 8 | (ip_vec[09] as u16);
+        let s5: u16 = (ip_vec[10] as u16) << 8 | (ip_vec[11] as u16);
+        let s6: u16 = (ip_vec[12] as u16) << 8 | (ip_vec[13] as u16);
+        let s7: u16 = (ip_vec[14] as u16) << 8 | (ip_vec[15] as u16);
+        // TODO refactor cursed code repetition
+        let ipv6 = std::net::Ipv6Addr::new(s0, s1, s2, s3, s4, s5, s6, s7);
+        let port_iter = it.take(PORT_SIZE);
+        let port_vec: Vec<u8> = port_iter.collect();
+        let port: u16 = (port_vec[0] as u16) | (port_vec[1] as u16) << 8;
+        // Obs.: there is a `to_ipv4_mapped` method on nightly
+        let ip = if let Some(ipv4) = ipv6.to_ipv4() {
+            std::net::IpAddr::V4(ipv4)
+        } else {
+            std::net::IpAddr::V6(ipv6)
+        };
+        Ok(Address::IP(ip, port))
+    }
+}
+
 impl<'a> Ser<'a> for Vec<Address> {
     fn ser_iter(self: &'a Self) -> U8IteratorBox<'a> {
         let size = self.len();
@@ -169,6 +203,28 @@ impl<'a> Ser<'a> for Vec<Address> {
                 Box::new(acc.chain(x.ser_iter()))
             });
         it
+    }
+}
+
+impl<'a> Deser<'a> for Vec<Address> {
+    fn deser_from_iter<I>(it: &mut I) -> Result<Self, DeserError>
+    where
+        I: Iterator<Item = u8>,
+    {
+        let size = it.next();
+        let size = if let Some(s) = size {
+            s
+        } else {
+            return Err(DeserError::Message(
+                "Missing address vector size byte.".into(),
+            ));
+        };
+        let mut addresses: Vec<Address> = Vec::with_capacity(size as usize);
+        for _ in 0..size {
+            let addr = Address::deser_from_iter(it)?;
+            addresses.push(addr);
+        }
+        Ok(addresses)
     }
 }
 
@@ -285,7 +341,11 @@ mod tests {
         assert_eq!(encoded[..],
             hex!("0228040d45e0e58100a42e08a43e95deaf10a400000000000000000000ffffc88955c810a4"));
 
-        println!("ips: {:?}", ips);
+        let mut stream = encoded.iter().copied();
+        let reconstructed =
+            Vec::<Address>::deser_from_iter(&mut stream).unwrap();
+
+        assert_eq!(addrs, reconstructed);
     }
 
     #[test]
