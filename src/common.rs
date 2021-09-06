c1c6bd631bcde75046911dab7b740fe4b57c0a54
Kelvin Santos <kelvinsteinersantos@gmail.com>
2021-09-06 10:09:46 -0300

    addresses serialization

--- src/common.rs
+++ src/common.rs
@@ -144,6 +144,27 @@ impl<'a> Ser<'a> for Address {
     }
 }
 
+impl<'a> Ser<'a> for Vec<Address> {
+    fn ser_iter(self: &'a Self) -> U8IteratorBox<'a> {
+        let size = self.len();
+        if size > 255 {
+            panic!("Address vector size is too big.")
+        }
+        let size = size as u8;
+
+        let base_it = std::iter::once(size);
+
+        // Then serialize all addresses
+        // TODO reduce overhead
+        let it = self
+            .iter()
+            .fold(Box::new(base_it) as U8IteratorBox<'a>, |acc, x| {
+                Box::new(acc.chain(x.ser_iter()))
+            });
+        it
+    }
+}
+
 // Message
 
 enum Message {
@@ -155,6 +176,7 @@ enum Message {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hex_literal::hex;
     use rand::prelude::*;
 
     #[test]
@@ -180,7 +202,7 @@ mod tests {
         }
 
         let mut stream = encoded.iter().copied();
-        let reconstructed = Post::deser_from_iter(&mut stream);
+        let reconstructed = Post::deser_from_iter(&mut stream).unwrap();
         assert_eq!(post, reconstructed);
         assert_eq!(post.hash(), post.hash());
         assert!(!stream.next().is_some());
@@ -207,5 +229,32 @@ mod tests {
         use std::default::Default;
         let genesis = Post::default();
         assert_eq!(genesis.hash(), U256::zero());
+    }
+
+    #[test]
+    fn addresses_ser() {
+        use std::net::IpAddr;
+        let ips: Vec<IpAddr> = vec![
+            "2804:d45:e0e5:8100:a42e:8a4:3e95:deaf".parse().unwrap(),
+            "200.137.85.200".parse().unwrap(),
+        ];
+
+        let addrs: Vec<Address> = ips
+            .iter()
+            .map(|ip| Address::IP(ip.clone(), 42000))
+            .collect();
+
+        let encoded: Vec<u8> = addrs.ser_iter().collect();
+
+        for i in 0..encoded.len() {
+            println!("{:4}: {}", i, encoded[i]);
+            // print!("{:02x}", encoded[i]);
+        }
+        println!("");
+
+        assert_eq!(encoded[..],
+            hex!("0228040d45e0e58100a42e08a43e95deaf00000000000000000000ffffc88955c8"));
+
+        println!("ips: {:?}", ips);
     }
 }
